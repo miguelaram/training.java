3d57fd6718e6f912853e6f7de8c71e19efb64344
Miguel A. Ramirez <miguelaustriaramirez@gmail.com>
2025-12-17 23:13:56 +0800

    pushed activity 2

---
+++ M2 Activity 2/Seatwork2.java
@@ -0,0 +1,12 @@
+package m2_Activity2;
+
+public class Seatwork2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Whiteboard wb = new Whiteboard();
+		int sum = wb.getSum(1,2);
+		System.out.println("Sum = " + String.valueOf(sum));
+	}
+
+}
---
+++ M2 Activity 2/Whiteboard.java
@@ -0,0 +1,8 @@
+package m2_Activity2;
+
+public class Whiteboard {
+	int getSum(int num1, int num2) {
+		int sum = num1 + num2;
+		return sum;
+	}
+}
